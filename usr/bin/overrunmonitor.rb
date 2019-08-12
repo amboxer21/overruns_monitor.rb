9d677194a6b24e8b8530c3a7c9b30799be15dbc2
Anthony Guevara <amboxer21@gmail.com>
2019-08-12 16:43:18 -0400

    Update overrunmonitor.rb
    Separating interfaces when overruns increment. This way the counter increments for each individual interface and not as a collective.

--- usr/bin/overrunmonitor.rb
+++ usr/bin/overrunmonitor.rb
@@ -1,58 +1,104 @@
 #!/usr/local/bin/ruby
  
-@round    = 0
-@count    = 0
-@hostname = `/bin/hostname`.gsub(/\n/,'')
-final_arr, old, arr_old, arr_new = [], [], [], []
+class OverrunsMonitor
 
-  def grab_range(command,array)
-    command.each_line {|x| array.push(x).flatten if x =~ /w[0-9]g1/ .. x =~ /TX/}
+  @@errors_found = false
+
+  ORIGINAL_STATE, INITIAL_STATE = [], []
+
+  def initialize
+
+    @final_state, @new_state = [], []
+    @final_rx_state, @final_tx_state = [], []
+
+    @round      = 0 # Loop iteration counter
+    @hostname   = `/bin/hostname`.gsub(/\n/,'')
+
+    @interfaces = {
+      "w1g1" => 0, "w2g1" => 0, "w3g1" => 0, "w4g1" => 0,
+      "w5g1" => 0, "w6g1" => 0, "w7g1" => 0, "w8g1" => 0,
+    }
+
   end
 
-  def push_values(old_array,final_array)
-    old_array.each {|lines| lines.scan(/overruns:[0-9]+/).to_s.scan(/\d+/).each {|x| final_array.push x}}
-  end
-
-  # Compare arrays for values that differ.
-  def self.incremented?(a,b)
-    a.each_index do |i|
-
-      if b[i] > a[i]
-         a[i] = b[i]
-         puts @count += 1
-      end
-
-      if @count == 5
-        ##puts "NOTICE: Overruns are incrementing on #{@hostname}."
-	`/bin/echo "NOTICE: Overruns are incrementing on #{@hostname}."  | /usr/bin/logger -t overruns`
-        exit
-      end
-
+  def query_interfaces(command,array)
+    command.each_line do |line|
+      array.push(line).flatten if line =~ /w[1-8]g1/ .. line =~ /TX/
     end
   end
 
-  # This is the initial push. It takes the overrun values and pushed them into an array
-  # These values will never change unless they differ from the second push inside the loop below. 
-  # In which case they will inherit the loop iterations values.
-  ##grab_range(ifconfig = `/bin/cat /home/aguevara/ifconfig.txt`,arr_old)
-  grab_range(ifconfig = `/sbin/ifconfig`,arr_old)
-  push_values(arr_old,old)
+  def populate_final_array(old_array,final_array,dx='RX')
+    old_array.each do |lines|
+      lines.scan(/\A\s+#{dx.upcase} packets.*(overruns:[0-9]+)/).to_s.scan(/\d+/).each do |line|
+        puts "line => (#{dx.upcase}) :- #{line}"
+        final_array.push line
+      end
+    end
+  end
+
+  # Compare arrays for values that differ.
+  def incremented?(original_array,new_array)
+    original_array.each_index do |index|
+      if new_array[index] > original_array[index]
+         original_array[index] = new_array[index]
+         @interfaces[@interfaces.keys[index]] += 1
+         # For testing when you want to see how many times an iface is incrementing.
+         puts "#{@interfaces.keys[index]} => #{@interfaces[@interfaces.keys[index]]}"
+      end
+
+      if @interfaces[@interfaces.keys[index]] == 10
+        puts "NOTICE: Overruns are incrementing on interface #{@interfaces.keys[index]} via #{@hostname}."
+	#`/bin/echo "NOTICE: Overruns are incrementing on #{@hostname}."  | /usr/bin/logger -t overruns`
+      end
+    end
+  end
+
+  # Main method
+  def monitor
+
+    while true do
  
-  while true do
+      ##ifconfig = `/sbin/ifconfig`
+      ifconfig = `/bin/cat /home/aguevara/ifconfig.txt`
+
+      # Checks values of overruns at each pass. They are cleared at the end of the loop. 
+      # After comparing to the initial values
+      query_interfaces(ifconfig,@new_state)
+      ##query_interfaces(ifconfig,@new_state)
+
+      populate_final_array(@new_state,@final_rx_state,'RX')
+      #populate_final_array(@new_state,@final_tx_state,'TX')
  
-    # Checks values of overruns at each pass. They are cleared at the end of the loop. 
-    # After comparing to the initial values
-    ##grab_range(ifconfig = `/bin/cat /home/aguevara/ifconfig.txt`,arr_new)
-    grab_range(ifconfig = `/sbin/ifconfig`,arr_new)
-    push_values(arr_new,final_arr)
+      incremented?(ORIGINAL_STATE,@final_rx_state)
+      #incremented?(ORIGINAL_STATE,@final_tx_state)
  
-    incremented?(old,final_arr)
- 
-    arr_new.clear
-    final_arr.clear
-    # 1 Second sleep in between checks.
-    sleep 1
-    # Check 5 times then exit
-    (@round == 6) ? (exit) : (@round += 1)
+      # clear all overrun tracking arrays
+      for state in [@new_state, @final_rx_state, @final_tx_state] do
+        state.clear
+      end
+
+      # 1 Second sleep in between checks.
+      sleep 15
+
+      # Check 10 times then exit
+      (@round == 11) ? (exit) : (@round += 1)
+
+    end
 
   end
+
+end
+
+##ifconfig = `/sbin/ifconfig`
+ifconfig = `/bin/cat /home/aguevara/ifconfig.txt`
+
+overruns = OverrunsMonitor.new
+
+# This is the initial push. It takes the overrun values and pushed them into an array
+# These values will never change unless they differ from the second push inside the loop below. 
+# In which case they will inherit the loop iterations values.
+overruns.query_interfaces(ifconfig,OverrunsMonitor::INITIAL_STATE)
+##overruns.query_interfaces(ifconfig,OverrunsMonitor::INITIAL_STATE)
+overruns.populate_final_array(OverrunsMonitor::INITIAL_STATE,OverrunsMonitor::ORIGINAL_STATE)
+
+overruns.monitor
