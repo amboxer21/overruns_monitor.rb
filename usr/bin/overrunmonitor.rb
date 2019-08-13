d268c691b6cee82d8193e9adbf57deeb19eef06d
Anthony Guevara <amboxer21@gmail.com>
2019-08-12 21:38:14 -0400

    Update overrunmonitor.rb
    Updated code. Notable change is that I added an array as the hash value with 2 items.

--- usr/bin/overrunmonitor.rb
+++ usr/bin/overrunmonitor.rb
@@ -14,13 +14,26 @@ class OverrunsMonitor
     @round      = 0 # Loop iteration counter
     @hostname   = `/bin/hostname`.gsub(/\n/,'')
 
+    # hash_structure = {interface => [count, skip_tx_overrun]}
     @interfaces = {
-      "w1g1" => 0, "w2g1" => 0, "w3g1" => 0, "w4g1" => 0,
-      "w5g1" => 0, "w6g1" => 0, "w7g1" => 0, "w8g1" => 0,
+      "w1g1" => [0, false], "w2g1" => [0, false], "w3g1" => [0, false], "w4g1" => [0, false],
+      "w5g1" => [0, false], "w6g1" => [0, false], "w7g1" => [0, false], "w8g1" => [0, false],
     }
 
   end
 
+  def reset_interface_boolean_values(nested_hash)
+    nested_hash.each_with_index do |interface,index|
+      nested_hash[nested_hash.keys[index]][1] = false
+    end
+  end
+
+  # This method queries the interface up until the TX line
+  # SAMPLE OUTPUT BELOW:
+  #wXg1 Link encap:Point-to-Point Protocol
+  #UP POINTOPOINT RUNNING NOARP  MTU:8  Metric:1
+  #RX packets:22551337 errors:0 dropped:0 overruns:17 frame:8
+  #TX packets:22551308 errors:0 dropped:0 overruns:59 carrier:5
   def query_interfaces(command,array)
     command.each_line do |line|
       array.push(line).flatten if line =~ /w[1-8]g1/ .. line =~ /TX/
@@ -30,26 +43,29 @@ class OverrunsMonitor
   def populate_final_array(old_array,final_array,dx='RX')
     old_array.each do |lines|
       lines.scan(/\A\s+#{dx.upcase} packets.*(overruns:[0-9]+)/).to_s.scan(/\d+/).each do |line|
-        puts "line => (#{dx.upcase}) :- #{line}"
         final_array.push line
       end
     end
   end
 
+  def push_xymon_notice(interface="")
+    puts "NOTICE: Overruns are incrementing on interface #{interface} via #{@hostname}."
+    #`/bin/echo "NOTICE: Overruns are incrementing on interface #{interface} via #{@hostname}."  | /usr/bin/logger -t overruns`
+  end
+
   # Compare arrays for values that differ.
   def incremented?(original_array,new_array)
     original_array.each_index do |index|
-      if new_array[index] > original_array[index]
+      if new_array[index] > original_array[index] and ! @interfaces[@interfaces.keys[index]][1]
          original_array[index] = new_array[index]
-         @interfaces[@interfaces.keys[index]] += 1
+         @interfaces[@interfaces.keys[index]][0] += 1
+         @interfaces[@interfaces.keys[index]][1] = true
          # For testing when you want to see how many times an iface is incrementing.
-         puts "#{@interfaces.keys[index]} => #{@interfaces[@interfaces.keys[index]]}"
+         puts "#{@interfaces.keys[index]} => #{@interfaces[@interfaces.keys[index]][0]}"
       end
 
-      if @interfaces[@interfaces.keys[index]] == 10
-        puts "NOTICE: Overruns are incrementing on interface #{@interfaces.keys[index]} via #{@hostname}."
-	#`/bin/echo "NOTICE: Overruns are incrementing on #{@hostname}."  | /usr/bin/logger -t overruns`
-      end
+      push_xymon_notice(@interfaces.keys[index]) if @interfaces[@interfaces.keys[index]][0] == 10
+
     end
   end
 
@@ -66,11 +82,13 @@ class OverrunsMonitor
       query_interfaces(ifconfig,@new_state)
       ##query_interfaces(ifconfig,@new_state)
 
-      populate_final_array(@new_state,@final_rx_state,'RX')
-      #populate_final_array(@new_state,@final_tx_state,'TX')
+      populate_final_array(@new_state,@final_rx_state,'RX') # RX arg not needed but used to balance out with the method below
+      populate_final_array(@new_state,@final_tx_state,'TX')
  
       incremented?(ORIGINAL_STATE,@final_rx_state)
-      #incremented?(ORIGINAL_STATE,@final_tx_state)
+      incremented?(ORIGINAL_STATE,@final_tx_state)
+
+      reset_interface_boolean_values(@interfaces)
  
       # clear all overrun tracking arrays
       for state in [@new_state, @final_rx_state, @final_tx_state] do
